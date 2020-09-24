b84473a2864f1e77b0189a72e6b0e7dd0fb8d168
NoKfi <makar-kukin@maril.ru>
2020-09-24 21:51:20 +0300

    Commit 1low-master

---
+++ app/src/main/java/com/example/taskmb/App.java
@@ -0,0 +1,47 @@
+package com.example.taskmb;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import data.AddDataBase;
+import data.TaskDao;
+
+public class App extends Application {
+
+    private AddDataBase dataBase;
+    private TaskDao taskDao;
+
+    private static App insance;
+
+    public static App getInstance(){
+        return insance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        dataBase = Room.databaseBuilder(getApplicationContext(),
+                AddDataBase.class,  "mtmdb")
+                .allowMainThreadQueries()
+                .build();
+        taskDao  = dataBase.noteDao();
+    }
+
+    public AddDataBase getDataBase(){
+        return dataBase;
+    }
+
+    public void setDataBase (AddDataBase dataBase){
+        this.dataBase = dataBase;
+    }
+
+    public TaskDao getTaskDao(){
+       return taskDao;
+    }
+
+    public void setTaskDao (TaskDao taskDao){
+        this.taskDao = taskDao;
+    }
+}
