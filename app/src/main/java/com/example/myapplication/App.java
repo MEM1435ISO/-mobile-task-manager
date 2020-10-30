c4ae81da4f307c8d8c8cd2fd0b4267de17105a92
Nearland <tima-makarov-1995@mail.ru>
2020-10-30 16:48:35 +0300

    Adds tags

---
+++ app/src/main/java/com/example/myapplication/App.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.example.myapplication.data.AppDatabase;
+import com.example.myapplication.data.NoteDao;
+
+public class App extends Application {
+
+    private AppDatabase database;
+    private NoteDao noteDao;
+
+    private static App insance;
+
+    public static App getInstance() {
+        return insance;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        insance = this;
+
+        database = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "db")
+                .allowMainThreadQueries()
+                .build();
+        noteDao = database.noteDao();
+    }
+
+   public AppDatabase getDatabase(AppDatabase database){
+       return database;
+   }
+
+   public void setDatabase(AppDatabase database){
+        this.database = database;
+   }
+
+   public NoteDao getNoteDao(){
+       return noteDao;
+   }
+
+   public void setNoteDao(NoteDao noteDao){
+       this.noteDao = noteDao;
+   }
+}
